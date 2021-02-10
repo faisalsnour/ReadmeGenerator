e55a838809842b4e0d2025b512a31ff1896bd665
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-10 00:22:59 -0500

    code to create readme file has been created and a result of generated readme file is attached

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ReadmeGenerator
+### random
+
+## Description
+
+kd skjdf j dfj dkfjdfk j
+
+## Installation
+
+kdjf dfkj kdjf ijdf jd fk
+
+## Usage
+
+kddkfkdkd
+
+## Contributing
+
+ifiifififif
+
+## Tests
+
+iuteun dkj df
+
+## License
+
+MIT
+
+## Questions
+
+[faisalsnour](https://github.com/faisalsnour)
+Reach out to me at faisalsnour@gmail.com
---
+++ index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'enter the project description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'enter the installation instruction',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'enter the usage of project',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'enter the contribution guidelines',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'enter test instructions',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'choose license from the list',
+        choices: ['BSD', 'MIT', 'GPL'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'enter your github username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'enter your email address',
+        name: 'email',
+    }
+  ])
+  .then(answers => {
+    // console.log(`the project name is`, answers.title);
+    fs.writeFile('readme.md', `### ${answers.title}\n## Description\n${answers.description}\n## Installation\n${answers.installation}\n## Usage\n${answers.usage}\n## Contributing\n${answers.contribution}\n## Tests\n${answers.test}\n## License\n${answers.license}\n## Questions\n[${answers.github}](https://github.com/${answers.github})\nReach out to me at ${answers.email} `, (err) =>
+     err ? console.error(err) : console.log('Success!')
+);
+    // Use user feedback for... whatever!!
+  })
